7fc2dfd2016adfcd7001abddd12af198f2df11a2
karolsitkowski <karol.sitkowski@gmail.com>
2017-05-22 13:44:16 +0200

    Initial commit

---
+++ src/main/java/pl/binary/App.java
@@ -0,0 +1,17 @@
+package pl.binary;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+    public static int toDecimal(int integer){
+        return 0;
+    }
+}
---
+++ src/test/java/pl/binary/AppTest.java
@@ -0,0 +1,33 @@
+package pl.binary;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class AppTest {
+
+    @Test
+    public void checkAllFifteenNumbers() {
+
+        assertEquals(0000,App.toDecimal(0));
+        assertEquals(0001,App.toDecimal(1));
+        assertEquals(0010,App.toDecimal(2));
+        assertEquals(0011,App.toDecimal(3));
+        assertEquals(0100,App.toDecimal(4));
+        assertEquals(0101,App.toDecimal(5));
+        assertEquals(0110,App.toDecimal(6));
+        assertEquals(0111,App.toDecimal(7));
+        assertEquals(1000,App.toDecimal(8));
+        assertEquals(1001,App.toDecimal(9));
+        assertEquals(1010,App.toDecimal(10));
+        assertEquals(1011,App.toDecimal(11));
+        assertEquals(1100,App.toDecimal(12));
+        assertEquals(1101,App.toDecimal(13));
+        assertEquals(1110,App.toDecimal(14));
+        assertEquals(1111,App.toDecimal(15));
+    }
+
+    @Test
+    public void ifInputIntIsNotFrom0to15() {
+}
